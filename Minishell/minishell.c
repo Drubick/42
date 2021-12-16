d8147b8c0e881fca62e968d281c181975fca2463
Valentin Nastase <vnastase@c3r12s5.42madrid.com>
2021-12-16 16:40:54 +0100

    Added minishell with error indication to fix

---
+++ Minishell/minishell.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ptorres <ptorres@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/23 12:02:38 by pablo             #+#    #+#             */
+/*   Updated: 2021/12/14 18:37:19 by ptorres          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "srcs/minishell.h"
+
+void	__init_vars(t_data *data, char **env)
+{
+	data->raw_cmd = "";
+	data->status = 0;
+	data->env = env;
+	data->exportables = save_exportables(env);
+	data->first_env = 1;
+	data->session_env = NULL;
+	data->asignation = 0;
+	g_status = 0;
+	getcwd(data->path, 2000);
+}
+
+void	main_exec(t_data *data, char **path_aux)
+{
+	if (ft_strlen(data->raw_cmd) > 0)
+	{
+		if (parse_comands(data))
+			execute_commands(data);
+		ft_bi_free(data->paths), free(*path_aux);
+		*path_aux = get_env_ms(data, "PATH");
+		data->paths = ft_split(*path_aux, ':');
+	}
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	t_data	data;
+	char	*path_aux;
+
+	argc = 0;
+	argv = NULL;
+	__init_vars(&data, env);
+	path_aux = get_env_ms(&data, "PATH");
+	data.paths = ft_split(path_aux, ':');
+	while (1)
+	{
+		if (g_status != 130)
+			g_status = 0;
+		signal(SIGQUIT, handle_sigquit2);
+		signal(SIGINT, handle_sigint);
+		data.cmds = NULL;
+		data.raw_cmd = readline("🐐inishell$ ");
+		if (!data.raw_cmd)
+			printf(""), exit(0);
+		if (*data.raw_cmd)
+			add_history(data.raw_cmd);
+		main_exec(&data, &path_aux);
+		free_command(&data);
+		free(data.raw_cmd);
+	}
+	return (0);
+}
---
+++ Minishell/srcs/builtins/echo.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ptorres <ptorres@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/09 17:07:23 by pablo             #+#    #+#             */
+/*   Updated: 2021/12/13 14:34:26 by ptorres          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	_init_vars(int *i, int *n, int *fd)
+{
+	*fd = 1;
+	*i = 1;
+	*n = 0;
+}
+
+int	is_flag(char *opt)
+{
+	int	i;
+
+	if (!opt || (opt[0] != '-'))
+		return (0);
+	i = 1;
+	while (opt[i] == 'n')
+		i++;
+	if (!opt[i])
+		return (1);
+	return (0);
+}
+
+void	execute_echo(t_cmds *cmd)
+{
+	int	i;
+	int	n;
+	int	fd;
+	int	aux;
+
+	_init_vars(&i, &n, &fd), n = 0, aux = 1;
+	if (cmd->otput_fd)
+		fd = cmd->otput_fd;
+	while (cmd->options[i])
+	{
+		if (is_flag(cmd->options[i]) && aux)
+			n = 1;
+		else
+		{
+			ft_putstr_fd(cmd->options[i], fd), aux = 0;
+			if (cmd->options[i + 1])
+				ft_putchar_fd(' ', fd);
+		}
+		i++;
+	}
+	if (!n)
+		ft_putchar_fd('\n', fd);
+	exit(0);
+}
